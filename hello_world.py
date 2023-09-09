0080b6bcd126b67a5f9e64b86cca8edd9093030d
Muhammed Saleh <muhammedsaleh@Muhammeds-MacBook-Air.local>
2023-09-09 15:16:55 -0300

    added selection sort

--- hello_world.py
+++ hello_world.py
@@ -16,3 +16,14 @@ def bubble_sort(arr):
             if(arr[j]>arr[j+1]):
                 arr[j],arr[j+1] = arr[j+1],arr[j]
     return arr
+
+# create a function for selection sort algorithm
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        min_index = i
+        for j in range(i+1,n):
+            if(arr[j]<arr[min_index]):
+                min_index = j
+        arr[i],arr[min_index] = arr[min_index],arr[i]
+    return arr
